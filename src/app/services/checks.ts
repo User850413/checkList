c0413cd65458ef11fb79d54259ae6406f26ce6fc
User850413 <csyou1228@gmail.com>
2024-11-22 18:02:26 +0900

    11/22 :sparkles: : postCheck 작성

--- src/app/services/checks.ts
+++ src/app/services/checks.ts
@@ -4,3 +4,12 @@ export async function getChecks() {
   const res = await axios.get('/api/checks');
   return res.data;
 }
+
+export async function postChecks({ task }: Partial<Check>) {
+  const res = await axios.post(
+    '/api/checks',
+    { task },
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+  return res.data;
+}
