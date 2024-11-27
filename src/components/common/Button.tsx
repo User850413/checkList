44d9fc9608b91c4ad314b083777292b78839cdbb
User850413 <csyou1228@gmail.com>
2024-11-27 15:56:28 +0900

    11/27 :sparkles: : button 컴포넌트 보완

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,14 +1,19 @@
 import { PropsWithChildren } from 'react';
+import clsx from 'clsx';
 
 interface ButtonProps extends PropsWithChildren {
   onClick: () => void;
+  className?: string;
 }
 
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button({ children, onClick, className }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex justify-center items-center bg-slate-200 rounded-md py-1 px-2 text-slate-800 shrink-0"
+      className={clsx(
+        'flex justify-center items-center bg-slate-200 rounded-md py-1 px-2 text-slate-800 shrink-0',
+        className
+      )}
     >
       {children}
     </button>
