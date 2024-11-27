d425911558be5f35b0de3244cbf3e43c63dac903
User850413 <csyou1228@gmail.com>
2024-11-27 14:16:01 +0900

    11/27 :sparkles: : tag api 함수 (post 및 delete) 추가

--- src/app/services/api/tags.ts
+++ src/app/services/api/tags.ts
@@ -6,12 +6,28 @@ export async function getAllTags() {
   return res.data;
 }
 
-export async function postTag() {}
+export async function postTag({ name }: Pick<Tag, 'name'>) {
+  if (!name?.trim()) throw new Error('task는 필수 입력값입니다');
+
+  try {
+    const res = await axios.post('/api/tags', {
+      name: name.trim(),
+    });
+    return res.data;
+  } catch (error) {
+    console.error(`체크 항목 생성 중 오류 발생 : ${error}`);
+    throw error;
+  }
+}
 
 export async function deleteTag({ _id }: Pick<Tag, '_id'>) {
   if (!_id?.trim()) throw new Error('id가 필요합니다');
 
-  // try{
-  //   const res = await axios.delete(`api/checks`)
-  // }
+  try {
+    const res = await axios.delete(`/api/tags?id=${_id}`);
+    return res.status;
+  } catch (error) {
+    console.error(`체크 항목 생성 중 오류 발생 : ${error}`);
+    throw error;
+  }
 }
