e2ee1fbc46fd7677503beb4f31e8d8ac068f40f4
User850413 <csyou1228@gmail.com>
2024-11-28 19:29:10 +0900

    11/28 :sparkles: : login API 엔드포인트 구현

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,50 @@
+import dbConnect from '@/app/lib/db/dbConnect';
+import User from '@/app/lib/db/models/users';
+import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        {
+          error: '이메일 및 비밀번호는 필수 입력사항입니다',
+        },
+        { status: 400 }
+      );
+    }
+
+    await dbConnect();
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: '존재하지 않는 email입니다' },
+        { status: 404 }
+      );
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return NextResponse.json(
+        {
+          error: '아이디 혹은 비밀번호가 일치하지 않습니다',
+        },
+        { status: 401 }
+      );
+    }
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+    return NextResponse.json(
+      { message: '로그인되었습니다', token },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 });
+  }
+}
