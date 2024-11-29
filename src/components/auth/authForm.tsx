27da361e35598a83efe4443bdccf8978419a30a2
User850413 <csyou1228@gmail.com>
2024-11-29 16:54:23 +0900

    11/29 :safety_vest: : 로그인폼 리렌더 최소화 및 유효성검사

--- src/components/auth/authForm.tsx
+++ src/components/auth/authForm.tsx
@@ -1,12 +1,74 @@
+'use client';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import InputBox from '../common/inputBox';
 import StyledButton from '../common/styledButton';
+import { emailCheck } from '@/app/utils/emailCheck';
 
 export default function AuthForm() {
+  const [email, setEmail] = useState<string | undefined>('');
+  const [pwd, setPwd] = useState<string | undefined>('');
+
+  const emailRef = useRef<null | HTMLInputElement>(null);
+  const pwdRef = useRef<null | HTMLInputElement>(null);
+
+  // 리렌더 방지 목적으로 참조할 value값
+  const emailValue = useRef(email);
+  const pwdValue = useRef(pwd);
+
+  useEffect(() => {
+    emailValue.current = email;
+    pwdValue.current = pwd;
+  }, [email, pwd]);
+
+  const onChangeEmailInput = useCallback(() => {
+    setEmail(emailRef.current?.value);
+  }, []);
+  const onChangePwdInput = useCallback(() => {
+    setPwd(pwdRef.current?.value);
+  }, []);
+
+  const handleSubmit = useCallback(() => {
+    if (!emailValue.current || !emailCheck(emailValue.current)) {
+      emailRef.current?.focus();
+      return;
+    } else if (!pwdValue.current) {
+      pwdRef.current?.focus();
+      return;
+    }
+  }, [emailValue, pwdValue]);
+
+  const handleEnterDown = useCallback(
+    (e: React.KeyboardEvent) => {
+      if (e.key === 'Enter') handleSubmit();
+    },
+    [handleSubmit]
+  );
+
   return (
     <div className="w-[480px] mx-auto bg-white shadow-card rounded-xl p-10 flex flex-col items-center gap-5">
-      <InputBox label="Email" type="email" required />
-      <InputBox label="비밀번호" type="password" required />
-      <StyledButton className="w-full">로그인</StyledButton>
+      <InputBox
+        label="Email"
+        type="email"
+        value={email}
+        onChange={onChangeEmailInput}
+        ref={emailRef}
+        required
+        autoFocus
+        onKeyDown={handleEnterDown}
+      />
+      <InputBox
+        label="비밀번호"
+        type="password"
+        value={pwd}
+        required
+        onChange={onChangePwdInput}
+        ref={pwdRef}
+        autoFocus
+        onKeyDown={handleEnterDown}
+      />
+      <StyledButton className="w-full" onClick={handleSubmit}>
+        로그인
+      </StyledButton>
     </div>
   );
 }
--- src/components/common/inputBox.tsx
+++ src/components/common/inputBox.tsx
@@ -1,19 +1,28 @@
 import { Input, InputProps } from '@chakra-ui/react';
 import { Field } from '../ui/field';
+import React from 'react';
 
 interface InputBoxProps extends InputProps {
   label?: string;
   helperText?: string;
 }
 
-export default function InputBox(props: InputBoxProps) {
-  const { helperText, label } = props;
+const InputBox = React.forwardRef<HTMLInputElement, InputBoxProps>(
+  function InputBox(props, ref) {
+    const { helperText, label, ...rest } = props;
 
-  return (
-    <>
-      <Field label={label} required helperText={helperText}>
-        <Input {...props} className="border-slate-100 border-2 rounded-lg" />
-      </Field>
-    </>
-  );
-}
+    return (
+      <>
+        <Field label={label} required helperText={helperText}>
+          <Input
+            {...rest}
+            ref={ref}
+            className="border-slate-100 border-2 rounded-lg"
+          />
+        </Field>
+      </>
+    );
+  }
+);
+
+export default React.memo(InputBox);
