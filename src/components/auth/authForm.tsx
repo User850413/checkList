6580442046309a3ff01a8cbb6b54349e1eea86ad
User850413 <csyou1228@gmail.com>
2024-11-29 15:47:38 +0900

    11/29 :recycle: : InputBox 코드 간소화

--- src/components/auth/authForm.tsx
+++ src/components/auth/authForm.tsx
@@ -4,8 +4,8 @@ import StyledButton from '../common/styledButton';
 export default function AuthForm() {
   return (
     <div className="w-[480px] mx-auto bg-white shadow-card rounded-xl p-10 flex flex-col items-center gap-5">
-      <InputBox label="Email" required />
-      <InputBox label="비밀번호" required />
+      <InputBox label="Email" type="email" required />
+      <InputBox label="비밀번호" type="password" required />
       <StyledButton className="w-full">로그인</StyledButton>
     </div>
   );
--- src/components/common/inputBox.tsx
+++ src/components/common/inputBox.tsx
@@ -1,31 +1,18 @@
-import { Input } from '@chakra-ui/react';
+import { Input, InputProps } from '@chakra-ui/react';
 import { Field } from '../ui/field';
 
-interface InputBoxProps {
+interface InputBoxProps extends InputProps {
   label?: string;
-  placeholder?: string;
-  size?: '2xl' | 'xl' | 'lg' | 'md' | 'sm' | 'xs' | '2xs';
   helperText?: string;
-  required?: boolean;
 }
 
-export default function InputBox({
-  label,
-  placeholder,
-  size,
-  helperText,
-  required = false,
-}: InputBoxProps) {
+export default function InputBox(props: InputBoxProps) {
+  const { helperText, label } = props;
+
   return (
     <>
       <Field label={label} required helperText={helperText}>
-        <Input
-          size={size}
-          placeholder={placeholder}
-          className="border-slate-100 border-2 rounded-lg"
-          type="email"
-          required={required}
-        />
+        <Input {...props} className="border-slate-100 border-2 rounded-lg" />
       </Field>
     </>
   );
