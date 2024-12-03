6d993fcb39ffc7124614de179e71f4ec3d0ab697
User850413 <csyou1228@gmail.com>
2024-12-03 13:37:51 +0900

    12/03 :sparkles: : interceptor 함수 추가

---
+++ src/app/services/token/interceptor.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const apiClient = axios.create({ baseURL: '/api' });
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
