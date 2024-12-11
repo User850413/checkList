8650ce9fe2543515aac61d22d22e978a9ea25d3c
User850413 <csyou1228@gmail.com>
2024-12-11 14:37:45 +0900

    12/11 :necktie: : 토큰 검증 미들웨어 작성

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,47 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  return NextResponse.json({ hello: 'hello' });
+  console.log(
+    `---------middleWare working on ${req.nextUrl.pathname}---------`
+  );
+  const accessToken = req.cookies.get('accessToken');
+  const refreshToken = req.cookies.get('refreshToken');
+
+  //NOTE : accessToken을 필요로 하지 않는 route 경로
+  const unAuthorizedPath = ['/api/register', '/api/login', '/api/logout'];
+  const isAuthorizationRequired = !unAuthorizedPath.some((path) =>
+    req.nextUrl.pathname.includes(path)
+  );
+  console.log(isAuthorizationRequired);
+
+  console.log(req.nextUrl.pathname);
+  console.log('--------');
+
+  //NOTE: accessToken 검증
+  if (isAuthorizationRequired) {
+    console.log('--------isAuthorizationRequired');
+    if (accessToken) {
+      console.log('--------accessToken');
+
+      const decodedValue = `Bearer ${decodeURIComponent(accessToken.value)}`;
+
+      const response = NextResponse.next();
+      response.headers.set('Authorization', decodedValue);
+      if (refreshToken) {
+        response.cookies.set('refreshToken', '', { maxAge: 0 }); // 쿠키 삭제
+      }
+
+      return response;
+    }
+    console.log('--------not isAuthorizationRequired');
+
+    return NextResponse.json({ error: 'error' });
+  } else {
+    console.log('else');
+    return NextResponse.next();
+  }
 }
 
 export const config = {
-  matcher: ['/api/:path*'],
+  matcher: ['/api/:path*', '/api/:path*/:path*'],
 };
