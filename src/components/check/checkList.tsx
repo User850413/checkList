bfc9fa74f5e04c54da2fc68680329ace36222e3e
User850413 <csyou1228@gmail.com>
2025-01-08 13:22:47 +0900

    01/08 :sparkles: : tag 삭제 시 modal 띄우기

--- src/components/check/checkList.tsx
+++ src/components/check/checkList.tsx
@@ -14,6 +14,15 @@ import TagNameInput from './tagNameInput';
 import FieldButton from '../common/fieldButton';
 import ProgressBar from '../common/progressBar';
 import { QueryKeys } from '@/app/lib/constants/queryKeys';
+import {
+  DialogActionTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogRoot,
+  DialogTrigger,
+} from '../ui/dialog';
+import { DialogBody, DialogHeader } from '@chakra-ui/react';
+import StyledButton from '../common/styledButton';
 
 interface CheckListProp {
   tagName: string;
@@ -76,48 +85,63 @@ function CheckList({ tagName, tagId, interest }: CheckListProp) {
   if (isError) return <div>Error</div>;
 
   return (
-    <div className="h-full w-full rounded-lg bg-white px-3 py-2">
-      <div className="border-b-2 border-b-slate-200">
-        <div className="flex items-center justify-between">
-          <TagNameInput tagName={tagName} tagId={tagId} />
-          <button
-            onClick={() => onClickDelete(tagId)}
-            className="inline-block p-1 text-slate-400"
-          >
-            x
-          </button>
-        </div>
-        <div className="flex w-full items-center gap-2 py-2">
-          <FieldButton fieldName={interest} />
-          <div className="flex w-full flex-col items-start justify-between gap-2">
-            <span className="cursor-default text-xs text-gray-500">
-              {renderedRate === 100 ? '완료!' : '진행도'}
-            </span>
-            <ProgressBar completed={renderedRate} />
+    <>
+      <div className="h-full w-full rounded-lg bg-white px-3 py-2">
+        <div className="border-b-2 border-b-slate-200">
+          <div className="flex items-center justify-between">
+            <TagNameInput tagName={tagName} tagId={tagId} />
+            <DialogRoot placement={'center'}>
+              <DialogTrigger>
+                <button className="inline-block p-1 text-slate-400">x</button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>정말로 삭제하시겠습니까?</DialogHeader>
+                <DialogFooter>
+                  <StyledButton
+                    onClick={() => onClickDelete(tagId)}
+                    color="red"
+                  >
+                    삭제
+                  </StyledButton>
+                  <DialogActionTrigger>
+                    <StyledButton>취소</StyledButton>
+                  </DialogActionTrigger>
+                </DialogFooter>
+              </DialogContent>
+            </DialogRoot>
+          </div>
+          <div className="flex w-full items-center gap-2 py-2">
+            <FieldButton fieldName={interest} />
+            <div className="flex w-full flex-col items-start justify-between gap-2">
+              <span className="cursor-default text-xs text-gray-500">
+                {renderedRate === 100 ? '완료!' : '진행도'}
+              </span>
+              <ProgressBar completed={renderedRate} />
+            </div>
           </div>
         </div>
+        <ul className="my-3 flex flex-col gap-2">
+          {checkList?.map((check, index) => (
+            <li key={check._id || index}>
+              <CheckListCard
+                id={check._id}
+                task={check.task}
+                isCompleted={check.isCompleted}
+                tagId={check.tagId}
+              />
+            </li>
+          ))}
+        </ul>
+        {checkList.length === 0 && (
+          <span className="mb-5 flex min-h-10 w-full cursor-default items-center justify-center text-sm text-slate-600">
+            아직 작성된 리스트가 없습니다. 항목을 추가해보세요!
+          </span>
+        )}
+        <div>
+          <CheckInput tagId={tagId} />
+        </div>
       </div>
-      <ul className="my-3 flex flex-col gap-2">
-        {checkList?.map((check, index) => (
-          <li key={check._id || index}>
-            <CheckListCard
-              id={check._id}
-              task={check.task}
-              isCompleted={check.isCompleted}
-              tagId={check.tagId}
-            />
-          </li>
-        ))}
-      </ul>
-      {checkList.length === 0 && (
-        <span className="mb-5 flex min-h-10 w-full cursor-default items-center justify-center text-sm text-slate-600">
-          아직 작성된 리스트가 없습니다. 항목을 추가해보세요!
-        </span>
-      )}
-      <div>
-        <CheckInput tagId={tagId} />
-      </div>
-    </div>
+    </>
   );
 }
 
