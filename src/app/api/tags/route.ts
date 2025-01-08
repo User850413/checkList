38df8f03c9fd12f9775fa169d35804f5281aafb0
User850413 <csyou1228@gmail.com>
2025-01-08 17:19:49 +0900

    01/08 :neckite: : tags get 엔드포인트 필터링 로직 수정

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -34,32 +34,32 @@ export async function GET(req: NextRequest) {
     let tags: TagType[] = [];
     let total = null;
 
-    // NOTE : interest 필터링
-    const interest = req.nextUrl?.searchParams.get('interest');
+    const filter: Record<string, any> = {};
+
+    const isCompleted = req.nextUrl.searchParams.get('isCompleted');
+    const interest = req.nextUrl.searchParams.get('interest');
+
+    if (isCompleted) {
+      if (isCompleted !== 'true' && isCompleted !== 'false')
+        return NextResponse.json(
+          { error: ERROR_MESSAGES.TYPE_BOOLEAN_ERROR.ko },
+          { status: 500 },
+        );
+      filter.isCompleted = isCompleted === 'true';
+    }
     if (interest) {
-      // NOTE : interest 있는지 검사
-      const isExistedInterest: interest | null = await Interest.findOne({
-        name: interest,
-      });
-      if (!isExistedInterest)
+      const conversedInterest = await Interest.findOne({ name: interest });
+      if (!conversedInterest)
         return NextResponse.json(
           { error: ERROR_MESSAGES.NOT_FOUND_INTEREST.ko },
           { status: 400 },
         );
 
-      // NOTE : interest 필터링
-      tags = await Tag.find({
-        interest: isExistedInterest._id,
-      })
-        .skip(skip)
-        .limit(limit)
-        .lean<TagType[]>();
-
-      total = await Tag.countDocuments({ interest: isExistedInterest._id });
-    } else {
-      tags = await Tag.find().skip(skip).limit(limit).lean<TagType[]>();
-      total = await Tag.countDocuments();
+      filter.interest = conversedInterest._id;
     }
+
+    tags = await Tag.find(filter).skip(skip).limit(limit).lean<TagType[]>();
+    total = await Tag.find(filter).countDocuments();
 
     // NOTE : tag response에 completedRate 추가
     const tagsWithRates = await Promise.all(
