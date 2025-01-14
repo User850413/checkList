59f91ac940399796711f6eecad0359da996542b4
User850413 <csyou1228@gmail.com>
2025-01-14 14:00:36 +0900

    01/14 :card_file_box: : tag 스키마에 userId 삭제

--- src/app/lib/db/models/tags.js
+++ src/app/lib/db/models/tags.js
@@ -2,11 +2,6 @@ import mongoose from 'mongoose';
 
 const tagSchema = new mongoose.Schema({
   name: { type: String, default: 'DEFAULT', required: true },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
   interest: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Interest',
