9371b16b7823477d1b6972075a8e4bcfc4490602
User850413 <csyou1228@gmail.com>
2025-01-22 17:59:28 +0900

    01/22 :sparkles: : 상단으로 가는 화살표 버튼 추가

--- src/app/(pages)/(checks)/my-list/page.tsx
+++ src/app/(pages)/(checks)/my-list/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { TagInput } from '@/components/check/tagInput';
 import FloatingButton from '@/components/common/floatingButton';
 import TagBundle from '@/components/tag/tagBundle';
@@ -12,24 +14,35 @@ import Image from 'next/image';
 
 export default function MyList() {
   const PLUS = `${process.env.PUBLIC_URL || ''}/icons/plus.svg`;
+  const ARROW_TOP = `${process.env.PUBLIC_URL || ''}/icons/arrow-top.svg`;
 
   return (
     <>
-      <DialogRoot placement={'center'}>
-        <DialogTrigger>
-          <FloatingButton>
-            <span className="relative h-full w-full">
-              <Image src={PLUS} alt="새 태그 추가" fill />
-            </span>
-          </FloatingButton>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogBody>
-            <TagInput />
-          </DialogBody>
-        </DialogContent>
-      </DialogRoot>
-      <main>
+      <div className="flex flex-col items-center">
+        <FloatingButton
+          classNames="bottom-20"
+          onClickFn={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          <span className="relative h-6 w-6">
+            <Image src={ARROW_TOP} alt="상단으로" fill />
+          </span>
+        </FloatingButton>
+        <DialogRoot placement={'center'}>
+          <DialogTrigger>
+            <FloatingButton>
+              <span className="relative h-full w-full">
+                <Image src={PLUS} alt="새 태그 추가" fill />
+              </span>
+            </FloatingButton>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogBody>
+              <TagInput />
+            </DialogBody>
+          </DialogContent>
+        </DialogRoot>
+      </div>
+      <main className="pt-6">
         <TagBundle />
       </main>
     </>
