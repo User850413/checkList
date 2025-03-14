b49e40ed5ca0c258dadf442529e5b7e21c8940e5
User850413 <csyou1228@gmail.com>
2025-03-14 13:19:07 +0900

    03/14 :bug: : 리다이렉션 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@
 
 import { useEffect } from 'react';
 import { checkToken } from './services/api/status';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
   useEffect(() => {
-    checkToken().catch((error) => console.error(error));
+    const getStatus = async () => {
+      const result = await checkToken().catch((error) => console.error(error));
+      if (result.authentication === false) {
+        router.push('/landing');
+      } else {
+        router.push('/my-list');
+      }
+    };
+
+    getStatus();
   }, []);
 
   return <>Loading...</>;
